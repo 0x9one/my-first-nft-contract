7bbc94011759402332f531c511a6ec97f0e40869
byeweb2 <sy47525@gmail.com>
2022-09-01 11:05:41 +0100

    Write our nft mint function to generate 5 tokens with diffrents IDs

--- contracts/BwItems.sol
+++ contracts/BwItems.sol
@@ -1,4 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-contract BwItems {}
+// Import the standard implementation of ERC721
+import "@openzeppeline/contracts/token/ERC721/ERC721.sol";
+
+// Contract 'BwItems' _IS_  an 'ERC721' contract
+contract BwItems is ERC721 {
+    constructor() ERC721("byeweb2NftCollection", "BwN") {
+        // Mint 5 NFts
+        // Take Address, Nft ID
+        _mint(msg.sender, 1);
+        _mint(msg.sender, 2);
+        _mint(msg.sender, 3);
+        _mint(msg.sender, 4);
+        _mint(msg.sender, 5);
+    }
+}
